476e66ac83cfdce16228d2de67bda31e5f467a46
ton5556 <74088689+ton5556@users.noreply.github.com>
2025-06-07 12:20:03 +0700

    Update Simple_IR.ino

--- Simple_IR.ino
+++ Simple_IR.ino
@@ -1,9 +1,3 @@
-Protocol=PulseDistance Raw-Data=0x6000000 64 bits LSB first Gap=3276750us Duration=67450us
-Send on a 8 bit platform with: 
-    uint32_t tRawData[]={0x4E02002, 0x6000000};
-    IrSender.sendPulseDistanceWidthFromArray(38, 3500, 1650, 450, 1300, 450, 450, &tRawData[0], 64, PROTOCOL_IS_LSB_FIRST, <RepeatPeriodMillis>, <numberOfRepeats>);
+FB9CFE85 ON
 
-Protocol=PulseDistance Raw-Data=0x6000000 64 bits LSB first Gap=3276750us Duration=67450us
-Send on a 8 bit platform with: 
-    uint32_t tRawData[]={0x4E02002, 0x6000000};
-    IrSender.sendPulseDistanceWidthFromArray(38, 3450, 1700, 450, 1300, 450, 400, &tRawData[0], 64, PROTOCOL_IS_LSB_FIRST, <RepeatPeriodMillis>, <numberOfRepeats>);
+A129DBBF OFF
